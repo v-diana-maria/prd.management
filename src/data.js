8ec921927c49363cd042f0d4342e5397e0cf8d89
radarpok <vdiana.m8@gmail.com>
2022-05-02 13:01:39 +0200

    solving image problems

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-import room1 from './images/details-1.jpeg';
+import room1 from './images/details-1.png';
 import room2 from './images/details-2.jpeg';
 import room3 from './images/details-3.jpeg';
 import room4 from './images/details-4.jpeg';
